d0e9d351d1e3a470f54c1da9f19c29311c279b50
Rvi Rao <69068706+Rvirao@users.noreply.github.com>
2022-04-08 12:31:50 +0530

    Update blog.js

--- src/modules/screen/blog.js
+++ src/modules/screen/blog.js
@@ -2,8 +2,13 @@ import React from "react";
 
 function BlogComponent() {
   return (
-    <div className="">
-      <div>lnj</div>
+     <div className=" mx-10 lg:mx-auto mt-20 w-full lg:w-216.5  bg-white-100 text-grey-75">
+      <div className="flex flex-row  justify-between">
+        <h1 className="font-PoppinsBold text-ft8">Blog</h1>
+        <button className="font-PoppinsSemiBold text-ft4 py-2 px-5 bg-blue-100 text-white-100 rounded-lg ">
+          Add Blog
+        </button>
+      </div>
     </div>
   );
 }
